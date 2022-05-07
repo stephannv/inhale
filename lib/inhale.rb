01421da57980b8788e1ee8a56e8502d686b7d7b5
stephannv <3025661+stephannv@users.noreply.github.com>
2022-05-06 21:26:48 -0300

    🚀 Create gem folder

---
+++ lib/inhale.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "inhale/version"
+
+module Inhale
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/inhale/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Inhale
+  VERSION = "0.1.0"
+end
